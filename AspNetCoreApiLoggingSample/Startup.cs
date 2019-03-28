f04ebaa81dc270428d818937a4aa13645f3169ea
Maik Braga <maikmb@gmail.com>
2019-03-28 13:24:50 -0300

    Inicio de Repositorio
    Importação de Projeto

---
+++ AspNetCoreApiLoggingSample/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreApiLoggingSample.Data;
+using AspNetCoreApiLoggingSample.Middleware;
+using AspNetCoreApiLoggingSample.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreApiLoggingSample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration.GetConnectionString("default");
+            services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+            services.AddTransient<ApiLogService>();
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, AppDbContext appDbContext)
+        {
+            appDbContext.Database.EnsureCreated();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseMiddleware<ApiLoggingMiddleware>();
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# ASP . NET Core API Logging Sample
+
+Code related to article at [salslab].
+
+[salslab]: <https://salslab.com/a/safely-logging-api-requests-and-responses-in-asp-net-core>
